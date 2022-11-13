990262ef9483846b45a3e029d2dc9fda4aae7547
Trishita <trishitamukherjee86@gmail.com>
2022-11-14 00:08:22 +0530

    yes

--- first_last_position.cpp
+++ first_last_position.cpp
@@ -1,48 +1,65 @@
 #include<bits/stdc++.h>
-int firts(int a[],int n,int k,int low,int high)
+using namespace std;
+int first(int arr[], int x, int n)
 {
-	low=0, high=n-1; int first=-1,result;
-	while(low<=high)
-	{
-		int mid=(low+high)/2;
-		if(a[mid]==k)
-		{	first=mid;
-			int result=mid-1;
-		}
-		else if(a[mid]<k)
-		low=mid+1;
-		else
-		high=mid-1;
-	}
-			return result;
+    int low = 0, high = n - 1, res = -1;
+    while (low <= high) {
+ 
+        // Normal Binary Search Logic
+        int mid = (low + high) / 2;
+ 
+        if (arr[mid] > x)
+            high = mid - 1;
+        else if (arr[mid] < x)
+            low = mid + 1;
+ 
+        // If arr[mid] is same as x, we
+        // update res and move to the left
+        // half.
+        else {
+            res = mid;
+            high = mid - 1;
+        }
+    }
+    return res;
 }
-int last(int a[],int n,int k,int low,int high)
+ 
+/* If x is present in arr[] then returns
+the index of LAST occurrence of x in
+arr[0..n-1], otherwise returns -1 */
+int last(int arr[], int x, int n)
 {
-	 low=0,high=n-1; int last=-1,result;
-	while(low<=high)
-	{
-	int mid=(low+high)/2;
-		if(a[mid]==k)
-		{	last=mid;
-			int result=mid+1;
-		}
-		else if(a[mid]<k)
-		low=mid+1;
-		else
-		high=mid-1;
-	}
-	return result;
+    int low = 0, high = n - 1, res = -1;
+    while (low <= high) {
+ 
+        // Normal Binary Search Logic
+        int mid = (low + high) / 2;
+ 
+        if (arr[mid] > x)
+            high = mid - 1;
+        else if (arr[mid] < x)
+            low = mid + 1;
+ 
+        // If arr[mid] is same as x, we
+        // update res and move to the right
+        // half.
+        else {
+            res = mid;
+            low = mid + 1;
+        }
+    }
+    return res;
 }
+ 
+// Driver code
 int main()
 {
-	int a[]={2,4,4,4,3,8};
-	int n=sizeof(a)/sizeof(a[0]);
-	int k=4;
-	int low=0,high=n-1;
-	int f=first(a,n,k,low,high);
-	int l=last(a,n,k,low,high);
-	cout<<"first element="<<f<<" ";
-	cout<<"last element="<<l<<" ";
-	return 0;
+    int arr[] = { 1, 2, 2, 2, 2, 3, 4, 7, 8, 8 };
+    int n = sizeof(arr) / sizeof(int);
+ 
+    int x = 3;
+    cout << "First Occurrence = " << first(arr, x, n);
+    cout << "\nLast Occurrence = " << last(arr, x, n);
+ 
+    return 0;
 }
-	
