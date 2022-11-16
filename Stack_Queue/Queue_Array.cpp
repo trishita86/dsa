5b54342c24631b77e29bd8b70a232462f2091067
Trishita <trishitamukherjee86@gmail.com>
2022-11-16 19:09:54 +0530

    check

--- Stack_Queue/Queue_Array.cpp
+++ Stack_Queue/Queue_Array.cpp
@@ -1,137 +1,48 @@
-/*#include<iostream>
+//Insert element
+/*#include<bits/stdc++.h>
 using namespace std;
-int queue[100],n=100,front=-1,rear=-1;
-void push(int val)
+int main()
 {
-	if(rear>=n-1)
-	cout<<"Queue overflow"<<endl;
-	else
+	queue<string>q;
+	q.push("cat");
+	q.push("dog");
+	q.push("rat");
+	q.push("fox");
+	q.push("lion");
+	q.pop();
+	q.pop();
+	cout<<"queue elements"<<endl;
+	while(!q.empty())
 	{
-	if(front==-1)
-	{
-		
-		front=0;
-		rear=rear+1;
-		queue[rear]=val;
+		cout<<"elements="<<q.front()<<endl;
+		q.pop();
 	}
-}
-}
-void pop()
+	return 0;
+}*/
+//Remove Element
+#include<bits/stdc++.h>
+using namespace std;
+void display(queue<string>q)
 {
-	if(front==-1 || front>rear)
-	cout<<"Queue underflow"<<endl;
-	else
+	while(!q.empty())
 	{
-	cout<<"deleted elements"<<queue[front]<<endl;
-	front++;
-	}
-}
-void display()
-{
-	if(front==-1)
-	cout<<"Queue empty"<<endl;
-	else
-	{
-		cout<<"elements in Queue"<<endl;
-		for(int i=front;i<=rear;i=i+1)
-		{
-			cout<<queue[i]<<endl;
-		}
+		cout<<q.front()<<endl;
+		q.pop();
 	}
 }
 int main()
 {
-	int ch,val;
-	cout<<"1.push in queue"<<endl;
-	cout<<"2.pop in queue"<<endl;
-	cout<<"3.display in queue"<<endl;
-	cout<<"4.exit"<<endl;
-	while(ch!=4)
-	{
-		cout<<"enter choice"<<endl;
-		cin>>ch;
-		switch(ch)
-		{
-			case 1:{
-				cout<<"enter value to be pushed"<<endl;
-				cin>>val;
-				push(val);
-				break;
-			}
-			case 2:{
-				pop();
-				break;
-			}
-			case 3:{
-				display();
-				break;
-			}
-			case 4:{
-				cout<<"exit"<<endl;
-				break;
-			}
-			default:{
-				cout<<"Invalid Choice"<<endl;
-			}
-		}
-	}
+	queue<string>q;
+	q.push("rat");
+	q.push("fox");
+	q.push("lion");
+	q.push("cat");
+	q.push("rabbit");
+	cout<<"inital queue"<<endl;
+	display(q);
+	q.pop();
+	q.pop();
+	cout<<"after deletion"<<endl;
+	display(q);
 	return 0;
-}*/
-#include <iostream>
-using namespace std;
-int queue[100], n = 100, front = - 1, rear = - 1;
-void Insert() {
-   int val;
-   if (rear == n - 1)
-   cout<<"Queue Overflow"<<endl;
-   else {
-      if (front == - 1)
-      front = 0;
-      cout<<"Insert the element in queue : "<<endl;
-      cin>>val;
-      rear++;
-      queue[rear] = val;
-   }
-}
-void Delete() {
-   if (front == - 1 || front > rear) {
-      cout<<"Queue Underflow ";
-      return ;
-   } else {
-      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
-      front++;
-   }
-}
-void Display() {
-   if (front == - 1)
-   cout<<"Queue is empty"<<endl;
-   else {
-      cout<<"Queue elements are : ";
-      for (int i = front; i <= rear; i++)
-      cout<<queue[i]<<" ";
-         cout<<endl;
-   }
-}
-int main() {
-   int ch;
-   cout<<"1) Insert element to queue"<<endl;
-   cout<<"2) Delete element from queue"<<endl;
-   cout<<"3) Display all the elements of queue"<<endl;
-   cout<<"4) Exit"<<endl;
-   do {
-      cout<<"Enter your choice : "<<endl;
-      cin>>ch;
-      switch (ch) {
-         case 1: Insert();
-         break;
-         case 2: Delete();
-         break;
-         case 3: Display();
-         break;
-         case 4: cout<<"Exit"<<endl;
-         break;
-         default: cout<<"Invalid choice"<<endl;
-      }
-   } while(ch!=4);
-   return 0;
 }
--- Stack_Queue/Stack_Aarays.cpp
+++ Stack_Queue/Stack_Aarays.cpp
@@ -1,76 +1,20 @@
-#include<iostream>
+//using stl.
+#include<bits/stdc++.h>
 using namespace std;
-int stack[100], n=100,top=-1;
-void push(int val)
-{
-	if(top>=n-1)
-	cout<<"Stack overflow"<<endl;
-	else
-	{
-		top++;
-		stack[top]=val;
-	}
-}
-void pop()
-{
-	if(top<=-1)
-	cout<<"stack underflow"<<endl;
-	else
-	{
-		cout<<"popped element="<<stack[top];
-		top--;
-	}
-}
-void display()
-{
-	if(top>=0)
-	{
-		cout<<"elements present in the stack are"<<endl;
-		for(int i=top;i>=0;i--)
-		{
-			cout<<stack[i]<<endl;
-		}
-	}
-	else
-	{
-		cout<<"stack is empty"<<endl;
-	}
-}
 int main()
 {
-	int ch,val;
-	cout<<"1.push in stack"<<endl;
-	cout<<"2.pop in stack"<<endl;
-	cout<<"3.display in stack"<<endl;
-	cout<<"4.exit"<<endl;
-	while(ch!=4)
+	stack<int>s;
+	s.push(10);
+	s.push(20);
+	s.push(30);
+	s.push(40);
+	s.push(50);
+	//s.pop();
+	//s.pop();
+	while(!s.empty())// for displaing all the elements.
 	{
-		cout<<"enter choice"<<endl;
-		cin>>ch;
-		switch(ch)
-		{
-			case 1:{
-				cout<<"enter value to be pushed"<<endl;
-				cin>>val;
-				push(val);
-				break;
-			}
-			case 2:{
-				pop();
-				break;
-			}
-			case 3:{
-				display();
-				break;
-			}
-			case 4:{
-				cout<<"exit"<<endl;
-				break;
-			}
-			default:{
-				cout<<"Invalid Choice"<<endl;
-			}
-		}
+		cout<<"top elements="<<s.top()<<endl;
+		s.pop();
 	}
 	return 0;
 }
